5af65f4d2d6a52a76d94a3883b539e91757f1e28
Mike Bannister <notimpossiblemike@gmail.com>
2012-09-03 12:51:51 -0400

    fixed and bumped

--- server.js
+++ server.js
@@ -2,11 +2,38 @@ var require = __meteor_bootstrap__.require;
 var path = require("path");
 
 NodeModules = {
-  _path: path.join(path.resolve('.'), 'public/node_modules'),
-  require: function(module) {
-    return require(path.join(NodeModules._path, module));
+  _path: null,
+
+  require: function(moduleName) {
+    var module;
+    
+    // try no path
+    try {
+      module = require(moduleName)
+    } catch (e) {}
+
+    // try public
+    if (!module) {
+      try {
+        var base = path.join(path.resolve('.'), 'public/node_modules')
+        module = require(path.join(base, moduleName));
+      } catch (e) {}
+    }
+    
+    if (!module && NodeModules._path) {
+      try {
+        var base = NodeModules._path;
+        module = require(path.join(base, moduleName));
+      } catch (e) {}
+    }
+    
+    return module;
   },
+
   setPath: function(modulePath) {
+    if (modulePath.indexOf('~') >= -1)
+      modulePath.replace('~', process.env.HOME);
+
     if (modulePath[0] === '/')
       NodeModules._path = modulePath;
     else
